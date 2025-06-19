87e7aff1ea6dc082f50ca639ce755aaf4b648473
student-ChestaVashishtha <student-ChestaVashishtha>
2025-06-19 22:06:52 +0530

    Changes

--- preprocessor.py
+++ preprocessor.py
@@ -7,7 +7,8 @@ import textblob
 # Download only once on first run
 nltk.download('punkt')
 nltk.download('stopwords')
-textblob.download_corpora()
+nltk.download('averaged_perceptron_tagger')
+
 
 def preprocess(data):
     pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s'
