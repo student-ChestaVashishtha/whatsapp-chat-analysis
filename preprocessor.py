1a586b5701b5ce90e5b398256cf5405e839c7adc
student-ChestaVashishtha <student-ChestaVashishtha>
2025-06-19 20:43:25 +0530

    adding nltk download

--- preprocessor.py
+++ preprocessor.py
@@ -1,6 +1,14 @@
 import re
 import pandas as pd
 from nrclex import NRCLex
+import nltk
+import textblob
+
+# Download only once on first run
+nltk.download('punkt')
+nltk.download('stopwords')
+textblob.download_corpora()
+
 def preprocess(data):
     pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s'
 
