6b00f5909f26ba5c5f3dc4eedc457c73023d4b71
student-ChestaVashishtha <student-ChestaVashishtha>
2025-06-19 22:26:35 +0530

    setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,2 @@
-# setup.sh
-mkdir -p ~/.streamlit/
-
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
+#!/bin/bash
+python -m textblob.download_corpora
